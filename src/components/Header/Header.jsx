f9ac86ef411bbd62f553637adaf09e28bb4601aa
pawan kumar <pawankumar@pawans-MacBook-Pro.local>
2023-08-20 11:04:00 +0530

    added: download movies function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { CSVLink } from "react-csv";
 import { MdHome, MdSearch, MdDownload } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useSelector } from "react-redux";
+import { get } from "../../utils/lodash";
 
 const Header = ({
   fromDetailsPage,
@@ -11,6 +14,36 @@ const Header = ({
 }) => {
   const navigate = useNavigate();
 
+  const csvRef = useRef();
+
+  const [downloadMovies, setDownloadMovies] = useState([]);
+
+  const { movies, isFetchingMovies } = useSelector((state) => state);
+
+  const onDownloadMovies = async () => {
+    if (isFetchingMovies) return;
+
+    const downloadDataArray = [];
+    if (movies && movies.length) {
+      movies.forEach((movie) => {
+        const movieObject = {};
+        movieObject["Name"] = get(movie, "original_title");
+        movieObject["Overview"] = get(movie, "overview");
+        movieObject["Release Date"] = get(movie, "release_date");
+        movieObject["Rating"] = get(movie, "vote_average");
+        downloadDataArray.push({ ...movieObject });
+      });
+    }
+
+    setDownloadMovies(downloadDataArray);
+
+    if (csvRef.current) {
+      setTimeout(() => {
+        csvRef.current.link.click();
+      }, 0);
+    }
+  };
+
   return (
     <header className={styles.Header__header}>
       {fromDetailsPage ? (
@@ -19,6 +52,12 @@ const Header = ({
         <div className={styles.Header__searchContainer}>
           <MdSearch
             className={`${styles.Header__searchIcon} ${styles.Header__icon}`}
+          />
+          <CSVLink
+            data={downloadMovies}
+            ref={csvRef}
+            style={{ display: "none" }}
+            filename="Movies.csv"
           />
           <input
             type="text"
@@ -36,6 +75,7 @@ const Header = ({
           onClick={() => navigate("/movies")}
         />
         <MdDownload
+          onClick={onDownloadMovies}
           className={`${styles.Header__icon} ${
             fromDetailsPage && styles.Header__displayNone
           }`}
